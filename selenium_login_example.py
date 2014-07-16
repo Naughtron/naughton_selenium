4a9bc5be73946d5d5b0dc75e5d8ba9d4de8cd293
cnaughton-carecloud <cnaughton@carecloud.com>
2014-07-15 22:19:21 -0400

    selenium GB login example

---
+++ selenium_login_example.py
@@ -0,0 +1,39 @@
+# imports
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys 
+
+# create a new instance of the browser 
+driver = webdriver.Firefox()
+
+# URL we are going to test 
+driver.get("http://www.giantbomb.com")
+
+#assert that bomb is in the title 
+assert "Giant Bomb" in driver.title 
+
+# find login button element on the page
+elem = driver.find_element_by_link_text('Log in')
+
+# click the login link
+elem.click()
+
+# assert that you are on the correct page 
+assert "Login" in driver.title
+
+# find the username and password edit boxes 
+username_box = driver.find_element_by_id('form__username')
+password_box = driver.find_element_by_id('form__password')
+#enter in the username 
+username_box.send_keys('usernamegoeshere')
+password_box.send_keys('userpasswordgoeshere')
+
+# find the login submit button 
+log_in_submit = driver.find_element_by_class_name('text-right')
+log_in_submit.click()
+
+
+
+
+
+
+
